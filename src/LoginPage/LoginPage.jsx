8d720197456dcd0a548c8860a515ceb7c60c8a11
satyam2318 <satyam2318@gmail.com>
2021-06-22 16:21:30 +0530

    login and register

--- src/LoginPage/LoginPage.jsx
+++ src/LoginPage/LoginPage.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-
 import { userActions } from '../_actions';
-
 class LoginPage extends React.Component {
     constructor(props) {
         super(props);
@@ -12,7 +10,7 @@ class LoginPage extends React.Component {
         this.props.logout();
 
         this.state = {
-            username: '',
+            email: '',
             password: '',
             submitted: false
         };
@@ -28,47 +26,85 @@ class LoginPage extends React.Component {
 
     handleSubmit(e) {
         e.preventDefault();
-
+        
         this.setState({ submitted: true });
-        const { username, password } = this.state;
-        if (username && password) {
-            this.props.login(username, password);
+        const { email, password } = this.state;
+        //console.log(email);
+        if (email && password) {
+            this.props.login(email, password);
         }
     }
+    
+
 
     render() {
         const { loggingIn } = this.props;
-        const { username, password, submitted } = this.state;
+        const { email, password, submitted } = this.state;
         return (
-            <div className="col-md-6 col-md-offset-3">
-                <h2>Login</h2>
-                <form name="form" onSubmit={this.handleSubmit}>
-                    <div className={'form-group' + (submitted && !username ? ' has-error' : '')}>
-                        <label htmlFor="username">Username</label>
-                        <input type="text" className="form-control" name="username" value={username} onChange={this.handleChange} />
-                        {submitted && !username &&
-                            <div className="help-block">Username is required</div>
-                        }
-                    </div>
-                    <div className={'form-group' + (submitted && !password ? ' has-error' : '')}>
-                        <label htmlFor="password">Password</label>
-                        <input type="password" className="form-control" name="password" value={password} onChange={this.handleChange} />
-                        {submitted && !password &&
-                            <div className="help-block">Password is required</div>
-                        }
-                    </div>
-                    <div className="form-group">
-                        <button className="btn btn-primary">Login</button>
-                        {loggingIn &&
+        <div className="row justify-content-center">
+            <div className="col-xl-10 col-lg-12 col-md-9">
+                <div className="card o-hidden border-0 shadow-lg my-5">
+                    <div className="card-body p-0">
+                        <div className="row">
+                            <div className="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                            <div className="col-lg-6">
+                                <div className="p-5">
+                                    <div className="text-center">
+                                        <h1 className="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                                    </div>
+                                    <form className="user" name="form" onSubmit={this.handleSubmit}>
+                                        <div className="form-group">
+                                            <input type="email" className="form-control form-control-user"
+                                                id="email" name="email" aria-describedby="emailHelp"
+                                                placeholder="Enter Email Address..." value={email} onChange={this.handleChange} />
+                                                {submitted && !email &&
+                                                <div className="help-block text-danger">Email is required</div>
+                                            }
+                                        </div>
+                                        <div className="form-group">
+                                            <input type="password" className="form-control form-control-user"
+                                                id="password" name="password" placeholder="Password" value={password} onChange={this.handleChange} />
+                                            {submitted && !password &&
+                                                <div className="help-block text-danger">Password is required</div>
+                                            }
+                                        </div>
+                                        
+                                        <button type="submit" className="btn btn-primary btn-user btn-block">
+                                            Login
+                                        </button>
+                                        {loggingIn && 
                             <img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
                         }
-                        <Link to="/register" className="btn btn-link">Register</Link>
+                                        {/* <hr/>
+                                        <a href="" className="btn btn-google btn-user btn-block">
+                                            <i className="fab fa-google fa-fw"></i> Login with Google
+                                        </a>
+                                        <a href="" className="btn btn-facebook btn-user btn-block">
+                                            <i className="fab fa-facebook-f fa-fw"></i> Login with Facebook
+                                        </a> */}
+                                    </form>
+                                    <hr/>
+                                    {/* <div className="text-center">
+                                        <a className="small" href="forgot-password.html">Forgot Password?</a>
+                                    </div>
+                                    */}
+                                    <div className="text-center">
+                                        <Link className="small" to="/register">Create an Account!</Link>
+                                    </div> 
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                </form>
+                </div>
+
             </div>
+
+        </div>
         );
     }
 }
+
+
 
 function mapState(state) {
     const { loggingIn } = state.authentication;
--- src/RegisterPage/RegisterPage.jsx
+++ src/RegisterPage/RegisterPage.jsx
@@ -1,28 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
 
+import { connect } from 'react-redux';
 import { userActions } from '../_actions';
+
 
 class RegisterPage extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = {
-            user: {
-                firstName: '',
-                lastName: '',
-                username: '',
-                password: ''
+        this.state  = {
+            user:{
+                fname:'',
+                lname:'',
+                email:'',
+                password:''
             },
             submitted: false
         };
-
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleChange(event) {
+    handleChange(event){
         const { name, value } = event.target;
         const { user } = this.state;
         this.setState({
@@ -38,7 +38,7 @@ class RegisterPage extends React.Component {
 
         this.setState({ submitted: true });
         const { user } = this.state;
-        if (user.firstName && user.lastName && user.username && user.password) {
+        if (user.fname && user.lname && user.email && user.password) {
             this.props.register(user);
         }
     }
@@ -47,46 +47,83 @@ class RegisterPage extends React.Component {
         const { registering  } = this.props;
         const { user, submitted } = this.state;
         return (
-            <div className="col-md-6 col-md-offset-3">
-                <h2>Register</h2>
-                <form name="form" onSubmit={this.handleSubmit}>
-                    <div className={'form-group' + (submitted && !user.firstName ? ' has-error' : '')}>
-                        <label htmlFor="firstName">First Name</label>
-                        <input type="text" className="form-control" name="firstName" value={user.firstName} onChange={this.handleChange} />
-                        {submitted && !user.firstName &&
-                            <div className="help-block">First Name is required</div>
-                        }
-                    </div>
-                    <div className={'form-group' + (submitted && !user.lastName ? ' has-error' : '')}>
-                        <label htmlFor="lastName">Last Name</label>
-                        <input type="text" className="form-control" name="lastName" value={user.lastName} onChange={this.handleChange} />
-                        {submitted && !user.lastName &&
-                            <div className="help-block">Last Name is required</div>
-                        }
-                    </div>
-                    <div className={'form-group' + (submitted && !user.username ? ' has-error' : '')}>
-                        <label htmlFor="username">Username</label>
-                        <input type="text" className="form-control" name="username" value={user.username} onChange={this.handleChange} />
-                        {submitted && !user.username &&
-                            <div className="help-block">Username is required</div>
-                        }
-                    </div>
-                    <div className={'form-group' + (submitted && !user.password ? ' has-error' : '')}>
-                        <label htmlFor="password">Password</label>
-                        <input type="password" className="form-control" name="password" value={user.password} onChange={this.handleChange} />
-                        {submitted && !user.password &&
-                            <div className="help-block">Password is required</div>
-                        }
-                    </div>
-                    <div className="form-group">
-                        <button className="btn btn-primary">Register</button>
-                        {registering && 
+            <div className="card o-hidden border-0 shadow-lg my-5">
+            <div className="card-body p-0">
+                <div className="row">
+                    <div className="col-lg-5 d-none d-lg-block bg-register-image"></div>
+                    <div className="col-lg-7">
+                        <div className="p-5">
+                            <div className="text-center">
+                                <h1 className="h4 text-gray-900 mb-4">Create an Account!</h1>
+                            </div>
+                            <form className="user" onSubmit={this.handleSubmit}>
+                                <div className="form-group row">
+                                    <div className="col-sm-6 mb-3 mb-sm-0">
+                                        <input type="text" className="form-control form-control-user" name="fname" id="fname"
+                                            placeholder="First Name" value={user.fname} onChange={this.handleChange} />
+                                            {submitted && !user.fname &&
+                                                <div className="help-block text-danger">first name is required</div>
+                                            }
+                                    </div>
+                                    <div className="col-sm-6">
+                                        <input type="text" className="form-control form-control-user" name="lname" id="lname"
+                                            placeholder="Last Name" value={user.lname} onChange={this.handleChange} />
+                                            {submitted && !user.lname &&
+                                                <div className="help-block text-danger">last name is required</div>
+                                            }
+                                    </div>
+                                </div>
+                                <div className="form-group">
+                                    <input type="email" className="form-control form-control-user" name="email" id="email"
+                                        placeholder="Email Address" value={user.email} onChange={this.handleChange} />
+                                        {submitted && !user.email &&
+                                                <div className="help-block text-danger">email is required</div>
+                                            }
+                                </div>
+                                <div className="form-group">
+                                    
+                                        <input type="password" className="form-control form-control-user"
+                                            id="password" name="password" placeholder="Password" value={user.password} onChange={this.handleChange} />
+                                            {submitted && !user.password &&
+                                                <div className="help-block text-danger">Password is required</div>
+                                            }
+                                    
+                                    {/* <div className="col-sm-6">
+                                        <input type="password" className="form-control form-control-user"
+                                            id="confirm_password" name="confirm_password" placeholder="Repeat Password" value={user.confirm_password} onChange={this.handleChange} />
+                                        {submitted && !user.confirm_password &&
+                                                <div className="help-block text-danger">Please confirm password</div>
+                                        }
+                                    </div> */}
+                                </div>
+                                <button type="submit" className="btn btn-primary btn-user btn-block">
+                                    Register Account
+                                </button>
+                                {registering && 
                             <img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
                         }
-                        <Link to="/login" className="btn btn-link">Cancel</Link>
+                               {/*
+                                <hr>
+                                <a href="index.html" className="btn btn-google btn-user btn-block">
+                                    <i className="fab fa-google fa-fw"></i> Register with Google
+                                </a>
+                                <a href="index.html" className="btn btn-facebook btn-user btn-block">
+                                    <i className="fab fa-facebook-f fa-fw"></i> Register with Facebook
+                                </a> */}
+                            </form>
+                            <hr/>
+                            {/* <div className="text-center">
+                                <a className="small" href="forgot-password.html">Forgot Password?</a>
+                            </div> */}
+                            <div className="text-center">
+                                <Link className="small" to="/login">Already have an account? Login!</Link>
+                            </div>
+                        </div>
                     </div>
-                </form>
+                </div>
             </div>
+        </div>
+
         );
     }
 }
